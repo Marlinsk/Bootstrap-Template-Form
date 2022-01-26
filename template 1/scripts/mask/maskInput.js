dd07b92fec5f7f1ea2012a73e5d5bc08ec7aa432
Marlon <Marlonnlmx@outlook.com>
2022-01-26 19:41:28 -0300

    Launching maskInput.js

---
+++ template 1/scripts/mask/maskInput.js
@@ -0,0 +1,8 @@
+// Máscaras de input
+$("#CPF").mask("000.000.000-00");
+$("#telefone-celular").mask("+00 (00) 0 0000-0000");
+$("#telefone-fixo").mask("+00 (00) 0000-0000");
+$("#CEP").mask("00000-000");
+$("#numero-do-banco").mask("000");
+$("#agencia").mask("0000");
+$("#conta-corrente").mask("0000000-0");
