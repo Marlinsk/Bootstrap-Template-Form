5a2d8d8f4cc5688741f0117ef224f80a4ff1ad97
Marlon <Marlonnlmx@outlook.com>
2022-02-01 14:12:15 -0300

    Update to code maskInput.js

--- template 1/scripts/mask/maskInput.js
+++ template 1/scripts/mask/maskInput.js
@@ -1,5 +1,29 @@
 // Máscaras de input
-$("#CPF").mask("000.000.000-00");
+$("#CPF-CNPJ").keydown(function(){
+    try {
+        $("#CPF-CNPJ").unmask();
+    } catch (e) {}
+
+    var tamanho = $("#CPF-CNPJ").val().length;
+
+    if(tamanho < 11){
+        $("#CPF-CNPJ").mask("999.999.999-99");
+    } else {
+        $("#CPF-CNPJ").mask("99.999.999/9999-99");
+    }
+
+    // ajustando foco
+    var elem = this;
+    setTimeout(function(){
+        // mudo a posição do seletor
+        elem.selectionStart = elem.selectionEnd = 10000;
+    }, 0);
+    // reaplico o valor para mudar o foco
+    var currentValue = $(this).val();
+    $(this).val('');
+    $(this).val(currentValue);
+});
+
 $("#telefone-celular").mask("+00 (00) 0 0000-0000");
 $("#telefone-fixo").mask("+00 (00) 0000-0000");
 $("#CEP").mask("00000-000");
