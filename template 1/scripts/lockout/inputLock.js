06a6ae3bf01b6c2a87d944b88ea2b1c65e7805ba
Marlon <Marlonnlmx@outlook.com>
2022-02-08 21:07:15 -0300

    Launching code

---
+++ template 1/scripts/lockout/inputLock.js
@@ -0,0 +1,27 @@
+$("#nome-completo", "#estado").on('keypress', function (event) {
+    var regex = new RegExp("^[a-zA-Z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇ ]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+      event.preventDefault();
+      return false;
+    }
+});
+  
+$("#cidade", "#bairro", "#rua", "#nome-do-banco").on('keypress', function (event) {
+    var regex = new RegExp("^[a-zA-Z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇ0-9 ]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+      event.preventDefault();
+      return false;
+    }
+});
+
+$("#numero-residencia").on('keypress', function (event) {
+    var regex = new RegExp("^[a-zA-Z0-9 ]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+      event.preventDefault();
+      return false;
+    }
+});
+  
